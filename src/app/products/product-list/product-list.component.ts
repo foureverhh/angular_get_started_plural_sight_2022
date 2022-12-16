5a59747d03bd5de38a1605c93370edd82ee362e5
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-12-16 08:32:20 +0100

    *ngIf *ngFor directive

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  pageTitle: string = 'Product List';
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "description 1",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "image1"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "description 1",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "image5"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
