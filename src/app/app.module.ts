5da66c7fc67799fec653f34a96e3c59444b6ed5b
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-12-16 22:33:05 +0100

    customized pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { ConvertToSpacePipe } from './pipes/convert-to-space.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe
   ],
   imports: [
     BrowserModule,
