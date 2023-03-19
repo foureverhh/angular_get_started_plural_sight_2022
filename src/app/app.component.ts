ae494e55487d38388b63ec4ef8e8602693837aad
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2023-03-19 21:00:20 +0100

    show all info on dataTable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TableServiceService } from './services/table-service.service';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface UserData {
+  id: string;
+  userId: string;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,11 +19,14 @@ import { TableServiceService } from './services/table-service.service';
 })
 export class AppComponent {
   pageTitle: string  = 'Acme Product Management';
-  posts: string[] = ['id','userId','title','body']
+  displayedColumns: string[] = ['id','userId','title','body'];
+  dataSource!: MatTableDataSource<UserData>; 
+  posts:any;
+
   constructor(private tableService: TableServiceService) {
     this.tableService.getData().subscribe(data => {
-      //this.posts = data;
-      console.log("data",data)
+      this.posts = data;
+      this.dataSource = new MatTableDataSource(this.posts)
     })
   }
 }
